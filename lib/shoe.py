dc96ac8add2e6ebf66e99619e9c59aaeae348f8e
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-09-03 15:12:46 +0300

    complete lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,22 @@
 
 class Shoe:
     pass
+
+    def __init__(self, brand, size):
+        self.size = size
+        self.brand = brand
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
